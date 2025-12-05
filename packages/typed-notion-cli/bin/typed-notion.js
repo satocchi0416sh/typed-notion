1eab7de3bdff30009d7e7b0f69a0777d5551b497
satocchi0416sh <satoyoshi44@gmail.com>
2025-12-05 11:01:00 +0900

    refactor: move typed-notion-cli to standalone repository

--- packages/typed-notion-cli/bin/typed-notion.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* global console, process */
-
-// ESM loader for the CLI
-import('../dist/index.js').catch(err => {
-  console.error('Failed to load typed-notion CLI:', err);
-  process.exit(1);
-});
